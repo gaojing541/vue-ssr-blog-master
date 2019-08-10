eb5253c47443f9d2220ad8a2a7db9a678394dca7
gaojing541 <574896013@qq.com>
2019-08-10 20:15:41 +0800

    还原大小写

--- admin/config/index.js
+++ admin/config/index.js
@@ -42,9 +42,8 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    // host: 'localhost', // can be overwritten by process.env.HOST
-    host: 'badmin.dirlir.com', // can be overwritten by process.env.HOST
-    port: 80, // can be overwritten by process.env.HOST, if port is in use, a free one will be determined
+    host: 'localhost', // can be overwritten by process.env.HOST
+    port: 8080, // can be overwritten by process.env.HOST, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
     notifyOnErrors: true,
